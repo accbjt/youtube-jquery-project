2b4351ce8f5b8ad174ca1e13cdcd82b82755afbd
Bill Tran <bill@billjohntran.com>
2017-10-11 22:46:46 -0700

    add initial files

---
+++ js/youtube.js
@@ -0,0 +1,3 @@
+// sample http request for youtube
+// https://www.googleapis.com/youtube/v3/search?pageToken=CDIQAA&part=snippet&q=lady+gaga&type=video&maxResults=25&key=
+// API https://developers.google.com/youtube/v3/docs/search/list
